3b764593ca40ae0fc9cf388dcc222831d4563133
akhilbevara <akhil.bevara@outlook.com>
2020-11-01 16:16:00 +0530

    Add files via upload

---
+++ build/precache-manifest.04e8c18b034752bd4384ea7f7dc9736d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0deabfdd6f1ea0ef633f565ca29f72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "275e61a273107433def3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1db3b5c1ea42c333471c",
+    "url": "/static/js/2.c5060ff1.chunk.js"
+  },
+  {
+    "revision": "0bc1c279bba15367e85e40877279bf17",
+    "url": "/static/js/2.c5060ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "275e61a273107433def3",
+    "url": "/static/js/main.8e3ee778.chunk.js"
+  },
+  {
+    "revision": "e05de519e6cd731943a3",
+    "url": "/static/js/runtime-main.92cf3ffd.js"
+  }
+]);
